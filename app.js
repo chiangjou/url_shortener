6473d4d5e0c3a9004aafef4da40dfcfc616ea02c
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-21 21:36:18 +0800

    feat: add form request

--- app.js
+++ app.js
@@ -28,13 +28,22 @@ db.once('open', () => {
   console.log('MongoDB connected!')
 })
 
+// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
+
 // routes setting
 // Home
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
   res.render('index')
 })
 
