8b0167ad2ddacc9ef9081de639d69f8b9568048b
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-21 23:14:13 +0800

    feat: revise urlSeeder.js

--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,19 +1,35 @@
-const mongoose = require('mongoose')
-const URL = require('./models/url')
+const db = require('../../config/mongoose')
+const URL = require('../url')
 
+// 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerCastLetters = upperCaseLetters.toLowerCase()
+const number = '0123456789'
+const charaterArray = upperCaseLetters.split('').concat(lowerCastLetters.split(''), concat(number.split('')))
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('MongoDB error!')
-})
-// 連線成功
+const generatedCharacter = (array) => {
+  let result = ''
+  for (i = 0; i < 5; i++) {
+    const index = Math.floor(Math.random() * array.length)
+    result += array[index]
+  }
+  return result
+}
+
 db.once('open', () => {
-  console.log('MongoDB connected!')
+  URL.create({
+    originalURL: 'https://www.google.com',
+    randomCharacter: generatedCharacter(charaterArray),
+    generatedURL: `http://localhost:3000/${generatedCharacter(charaterArray)}`
+  })
+  console.log('URL generated')
 })
+
+module.exports = {
+  charaterArray: charaterArray,
+  generatedCharacter: generatedCharacter
+}
