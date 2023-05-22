8a66963e3b688ac8f64e1c41b1bc1262b0ecab1a
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-22 23:36:08 +0800

    feat: add routes

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-// Include express from node_modules
 const express = require('express')
 const app = express()
-// require express-handlebars
 const exphbs = require('express-handlebars')
-// Define server related variables
+const routes = require('./routes')
 const port = 3000
-// require mongoose
 require('./config/mongoose')
 
-// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// setting static engine
 app.use(express.static('public'))
 
-// setting body-parser
 app.use(express.urlencoded({ extended: true }))
 
-// routes setting
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
-app.post('/', (req, res) => {
-  console.log('req.body', req.body)
-  res.render('index')
-})
-
-// start and listen on the Express server
 app.listen(3000, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+const CRUD = require('./modules/CRUD')
+
+router.use('/', home)
+router.use('/', CRUD)
+
+module.exports = router
