87f3609db79ff49af5e89a7fb7bbe2f7c207f3e4
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-24 19:45:13 +0800

    feat: delete routes

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const home = require('./modules/home')
-const CRUD = require('./modules/CRUD')
-
-router.use('/', home)
-router.use('/', CRUD)
-
-module.exports = router
