f7de797677cd7edac9fdcb33565b6be51f632717
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-24 19:27:35 +0800

    feat:revise config and models

--- models/url.js
+++ models/url.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const urlSchema = new Schema({
+  shortURL: {
+    type: String,
+    required: true
+  },
   originalURL: {
-    type: String,
-    required: true
-  },
-  randomCharacter: {
-    type: String,
-    required: true
-  },
-  transformedURL: {
     type: String,
     required: true
   },
