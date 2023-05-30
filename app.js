bbedd11c604af8b506727b7da06486b6ed4c3651
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-31 00:09:53 +0800

    feat: revise app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // Include express from node_modules
-const express = require('express')
+const express = require("express")
 // require express-handlebars here
-const exphbs = require('express-handlebars')
+const exphbs = require("express-handlebars")
 // Define server related variables
 const port = 3000
 // require mongoose
-require('./config/mongoose')
+require("./config/mongoose")
 // Include router
-const routes = require('./routes')
+const routes = require("./routes")
 
 const app = express()
 
 // setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 // setting static engine
-app.use(express.static('public'))
+app.use(express.static("public"))
 // Ask every request use body-parser
 app.use(express.urlencoded({ extended: true }))
 // Direct request to router
